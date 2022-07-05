45825eeb5c43a812eeb9cf9419587b772efa87e7
Gourav Upadhyay <99647524+tampurus@users.noreply.github.com>
2022-07-05 13:04:56 +0530

    Update Maximum Subarray.cpp

--- Apna College DSA sheet/Array/Maximum Subarray.cpp
+++ Apna College DSA sheet/Array/Maximum Subarray.cpp
@@ -22,3 +22,24 @@ public:
         return ans;
     }
 };
+
+/*
+second approach kaden's algo
+time -> O(N)
+space -> no extra space
+*/
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int n = nums.size();
+        if (n==1) return nums[0];
+        int sum=0,max_sum=INT_MIN;
+        int i,j,k;
+        for(i=0 ; i<n ; i++){
+            sum+=nums[i];
+            if(nums[i]>sum) sum=nums[i];
+            max_sum = max(max_sum,sum);
+        }
+        return max_sum;
+    }
+};
